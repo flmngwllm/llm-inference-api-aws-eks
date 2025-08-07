96d794f740b76d7b425818c190aac8491bd63dd3
flmngwllm <flmngwllm232@gmail.com>
2025-08-06 22:55:33 -0400

    created k8s.tf and added service account for alb

---
+++ terraform/k8s.tf
@@ -0,0 +1,15 @@
+resource "kubernetes_service_account" "secure_devops_alb_service_account" {
+  metadata {
+    name      = "aws-alb-controller"
+    namespace = "kube-system"
+    annotations = {
+      "eks.amazonaws.com/role-arn" = aws_iam_role.llm_inference_api_alb_controller_role.arn
+    }
+  }
+
+  automount_service_account_token = true
+
+  depends_on = [
+    time_sleep.delay_for_access_entry
+  ]
+}
