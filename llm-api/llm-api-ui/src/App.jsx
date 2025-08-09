9d8235679bdf7af1a12b421a4eb0f48475846e38
flmngwllm <flmngwllm232@gmail.com>
2025-08-08 20:05:23 -0400

    created llm-api-ui with React and Vite setup and added intial helm chart files - Added LlmComponent.jsx to display a placeholder heading.

---
+++ llm-api/llm-api-ui/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import LlmComponent from './llmComponent';
+
+function App() {
+  
+  return (
+    
+      <div>
+        <LlmComponent />
+      </div>
+    
+    
+  )
+}
+
+export default App
---
+++ llm-api/llm-api-ui/src/llmComponent.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react" 
+
+const LlmComponent = () => {
+    return (
+        <div>
+            <h1>LLm Here</h1>
+        </div>
+    )
+}
+
+
+export default LlmComponent;
